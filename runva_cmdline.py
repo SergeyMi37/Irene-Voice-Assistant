ff487b0766c51c9504bc7b0c938624a464a1df15
sergeymi37 <sergey.mikhaylenko@gmail..com>
2024-01-24 16:12:24 +0300

    upd cmd

--- runva_cmdline.py
+++ runva_cmdline.py
@@ -2,6 +2,7 @@ import traceback
 
 from vacore import VACore
 import time
+import json
 
 # ------------------- main loop ------------------
 if __name__ == "__main__":
@@ -21,12 +22,35 @@ if __name__ == "__main__":
         traceback.print_exc()
 
 
-    exit(0) # если нужно - закомментируйте и можно будет работать с командной строкой
-
-    print("Enter command (user text like 'привет') or 'exit'")
+    #exit(0) # если нужно - закомментируйте и можно будет работать с командной строкой
+    _msg="Enter command (user text like 'привет') or 'exit .,q' or '?' доступные команды"
+    print(_msg)
     while True:
         cmd = input("> ")
-        if cmd == "exit":
+        if cmd == "exit" or cmd=='.' or cmd==',' or cmd=='q':
             break
-
-        cmd_core.execute_next(cmd,cmd_core.context)
+        elif cmd=="":
+            print(_msg)
+        elif cmd=="?":
+            print(cmd_core)
+            _num=0
+            _promt={}
+            for _a in cmd_core.commands:
+                _num=_num+1
+                _promt[_num]=_a
+                print(_num,_a)
+            while True:
+                _num = input("Номер > ")
+                if _num=='':
+                    break
+                else:
+                    #print(_num)
+                    _val=_promt.get(int(_num))
+                    #print(_val)
+                    _va=_val.split('|')[0]
+                    if _va:
+                        cmd=_va
+                        print(_va)
+                        cmd_core.execute_next(cmd,cmd_core.context)
+        else:
+            cmd_core.execute_next(cmd,cmd_core.context)
